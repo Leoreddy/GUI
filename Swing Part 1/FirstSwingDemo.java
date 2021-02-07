fac270b3a3040dfddcc3090ccbe436a99816b703
Leoreddy <leoreddy05@gmail.com>
2021-02-07 07:29:06 +0200

    added } and space

--- Swing Part 1/FirstSwingDemo.java
+++ Swing Part 1/FirstSwingDemo.java
@@ -5,8 +5,11 @@ public class FirstSwingDemo{
 
         public static final int WIDTH = 300;
         public static final int HEIGHT = 200;
+
     public static void main (String [] args)
     {
         JFrame firstWindow = new JFrame();
         firstWindow.setSize(WIDTH,HEIGHT);
     }
+
+}
