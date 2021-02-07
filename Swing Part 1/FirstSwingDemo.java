3188542d88ba3c2c2f4b3f2356bd5ab5e27bef18
Leoreddy <leoreddy05@gmail.com>
2021-02-07 07:27:13 +0200

    added code

--- Swing Part 1/FirstSwingDemo.java
+++ Swing Part 1/FirstSwingDemo.java
@@ -3,7 +3,10 @@ import javax.swing.JButton;
 
 public class FirstSwingDemo{
 
+        public static final int WIDTH = 300;
+        public static final int HEIGHT = 200;
     public static void main (String [] args)
     {
-
+        JFrame firstWindow = new JFrame();
+        firstWindow.setSize(WIDTH,HEIGHT);
     }
