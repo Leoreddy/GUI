e1e2e21fd1040846fe319e7087201312aa01dd91
Leoreddy <leoreddy05@gmail.com>
2021-02-09 12:54:04 +0200

    Created FirstWindow.java

---
+++ Swing Part 1/FirstWindow.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+
+public class FirstWindow extends JFrame {
+
+    public static final int WIDTH = 300;
+    public static final int HEIGHT = 200;
+
+    public FirstWindow() {
+        super();
+        setTitle("First Window Class");
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        JButton endButton = new JButton("Click to end program.");
+        endButton.addActionListener(new EndingListener());
+        add(endButton);
+    }
+}
+
